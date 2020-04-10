29fdc523f50ee676bcc0d623f6d5156e9be4f1b5
chengcheng <1659017183@qq.com>
2020-04-10 09:23:17 +0800

    李成周五上午提交了

--- src/main/java/com/hy/crmsystem/mrli/service/IDocumentaryService.java
+++ src/main/java/com/hy/crmsystem/mrli/service/IDocumentaryService.java
@@ -1,7 +1,10 @@
 package com.hy.crmsystem.mrli.service;
 
+import com.hy.crmsystem.mrli.entity.BaseEntity;
+import com.hy.crmsystem.mrli.entity.DataGridView;
 import com.hy.crmsystem.mrli.entity.Documentary;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.hy.crmsystem.mrli.vo.DocumentaryVo;
 
 /**
  * <p>
@@ -13,4 +16,17 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface IDocumentaryService extends IService<Documentary> {
 
+    /**
+     * 查询全部跟单
+     * @param documentaryVo
+     * @return
+     */
+    DataGridView queryAllDocumentary(DocumentaryVo documentaryVo);
+
+
+    /**
+     * 新增跟单
+     * @param documentaryVo
+     */
+    void addDocumentary(DocumentaryVo documentaryVo);
 }
--- src/main/java/com/hy/crmsystem/mrli/service/impl/DocumentaryServiceImpl.java
+++ src/main/java/com/hy/crmsystem/mrli/service/impl/DocumentaryServiceImpl.java
@@ -1,14 +1,26 @@
 package com.hy.crmsystem.mrli.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.hy.crmsystem.mrli.entity.BaseEntity;
+import com.hy.crmsystem.mrli.entity.DataGridView;
 import com.hy.crmsystem.mrli.entity.Documentary;
 import com.hy.crmsystem.mrli.mapper.DocumentaryMapper;
 import com.hy.crmsystem.mrli.service.IDocumentaryService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hy.crmsystem.mrli.vo.DocumentaryVo;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * <p>
- *  服务实现类
+ * 服务实现类
  * </p>
  *
  * @author zhangduo
@@ -17,4 +29,31 @@ import org.springframework.stereotype.Service;
 @Service
 public class DocumentaryServiceImpl extends ServiceImpl<DocumentaryMapper, Documentary> implements IDocumentaryService {
 
+    private static final Logger LOGGER = Logger.getLogger(DocumentaryServiceImpl.class);
+
+    @Autowired
+    private DocumentaryMapper documentaryMapper;
+
+    @Override
+    public DataGridView queryAllDocumentary(DocumentaryVo documentaryVo) {
+//        IPage<Documentary> page = new Page<>(documentaryVo.getPage(), documentaryVo.getLimit());
+//        LOGGER.info(page.getTotal()+"--------------------------------");
+//        QueryWrapper<Documentary> qw = new QueryWrapper<>();
+//        qw.like(StringUtils.isNotEmpty(documentaryVo.getDocPeople()),"DocPeople",documentaryVo.getDocPeople());
+//        qw.like(StringUtils.isNotEmpty(documentaryVo.getDocTitle()), "DocTitle", documentaryVo.getDocTitle());
+//        /*qw.ge(documentaryVo.getDocTime() != null, "DocTime", documentaryVo.getDocTime());
+//        qw.le(documentaryVo.getEndTime() != null, "endtime", documentaryVo.getEndTime());*/
+//        qw.orderByDesc("DocTime");
+//        this.documentaryMapper.selectPage(page, qw);
+//        return new DataGridView(Long.parseLong(String.valueOf(page.getRecords().size())),page.getRecords());
+        Page<Object> page = PageHelper.startPage(documentaryVo.getPage(), documentaryVo.getLimit());
+        List<Documentary> data = this.documentaryMapper.queryAllDocumentary(documentaryVo);
+        return new DataGridView(page.getTotal(), data);
+    }
+
+
+    @Override
+    public void addDocumentary(DocumentaryVo documentaryVo) {
+        this.documentaryMapper.insert(documentaryVo);
+    }
 }
